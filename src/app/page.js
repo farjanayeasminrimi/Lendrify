a3dd9a9415b0dd8da660b733117755ed81796413
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-05-04 14:45:03 +0600

    added marquee

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Featured from "@/components/HomePage/Featured";
 import Hero from "@/components/HomePage/Hero";
+import MarqueeText from "@/components/Shared/MarqueeText";
 
 export default function Home() {
   return (
     <div className="my-12 py-10">
       {/* Hero Section */}
       <Hero></Hero>
+      <MarqueeText></MarqueeText>
       {/* Featured Section */}
       <Featured></Featured>
     </div>
---
+++ src/components/Shared/MarqueeText.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const MarqueeText = () => {
+  return (
+    <div className="container mx-auto bg bg-[#E6DED3] p-4 rounded-md flex gap-2.5 items-center">
+      <button className="btn bg-[#5F737A] hover:bg-[#4C5E64] text-white">Update :</button>
+      <Marquee pauseOnHover>
+        <p className="text-[#2F3A3D] font-medium">
+          New Arrivals: Atomic Habits &nbsp;|&nbsp; The Psychology of Money &nbsp;|&nbsp; Sapiens
+          &nbsp;|&nbsp; The Alchemist &nbsp;|&nbsp; Special Discount on Memberships —{" "}
+          <span className="text-orange-600">Up to 30% Off</span>&nbsp; | Limited Time
+          Offer!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; •••&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </p>
+      </Marquee>
+    </div>
+  );
+};
+
+export default MarqueeText;
