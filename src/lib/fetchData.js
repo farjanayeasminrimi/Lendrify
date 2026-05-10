46ac9cb263494218245f5f992cfeccb67b1904b6
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-05-10 21:56:13 +0600

    fixed deployment Isussues

--- src/lib/fetchData.js
+++ src/lib/fetchData.js
@@ -1,17 +1,32 @@
+import path from "path";
+import fs from "fs";
+
+const urlMaker = (fileName) => {
+  const filePath = path.join(process.cwd(), "public", fileName);
+  const fileContents = fs.readFileSync(filePath, "utf8");
+  const data = JSON.parse(fileContents);
+  return data;
+};
 export const booksData = async () => {
-  const res = await fetch("http://localhost:3000/data/data.json");
-  const data = await res.json();
-  return data.books;
+  // const res = await fetch("http://localhost:3000/data/data.json");
+  // const data = await res.json();
+  // return data.books;
+
+  const books = urlMaker("data.json");
+  return books.books;
 };
 export const categoriesData = async () => {
-  const res = await fetch("http://localhost:3000/data/data.json");
-  const data = await res.json();
+  // const res = await fetch("http://localhost:3000/data/data.json");
+  // const data = await res.json();
+  const data = urlMaker("data.json");
   return data.categories;
 };
 export const categoryData = async (slug = "all") => {
-  const res = await fetch("http://localhost:3000/data/data.json");
-  const data = await res.json();
+  // const res = await fetch("http://localhost:3000/data/data.json");
+  // const data = await res.json();
   // console.log(slug);
+
+  const data = urlMaker("data.json");
   if (slug === "all") {
     return data.books;
   }
