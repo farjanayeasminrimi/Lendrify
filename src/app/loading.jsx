ad01ef729888cae15fef7b7a79a0b5818c02c470
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-05-12 00:11:37 +0600

    loader added

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="min-h-screen mx-auto">
+    <div className="min-h-screen grid place-content-center mx-auto">
       <div className="flex gap-4">
-        <h1 className="text-4xl">Loading...</h1>
+        <h1 className="text-3xl font-bold text-[#2F3A3D]">Loading...</h1>
         <span className="loading loading-ring loading-xl"></span>
       </div>
     </div>
